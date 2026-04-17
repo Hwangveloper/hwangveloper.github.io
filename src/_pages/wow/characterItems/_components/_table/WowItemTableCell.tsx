b2f23bd99245c1c54038a1f40b42107737652665
Hwangveloper <cthrss@gmail.com>
2026-04-17 23:58:07 +0900

    fix: 템레벨 색상 문장구간으로 수정

--- src/_pages/wow/characterItems/_components/_table/WowItemTableCell.tsx
+++ src/_pages/wow/characterItems/_components/_table/WowItemTableCell.tsx
@@ -59,36 +59,52 @@ const WowItemTableCell: React.FC<WowItemTableCellProps> = ({ items, partType, on
   }
 
   const getItemLevelColor = () => {
-
+    const itemLevel = item?.level ?? 0;
+    const idx = typeItemLevels.findIndex((iLevel) => iLevel.itemLevel === itemLevel);
+console.log(itemLevel);
+console.log(item?.type);
+console.log(idx);
     switch (item?.type) {
       case EWowItemType.ADVENTURER:
         return "#ff9900";
       case EWowItemType.STAGER:
+        if (idx === 0) {
+          return "#ff9900";
+        }
         return "#ff0000";
       case EWowItemType.CHAMPION:
+        if (idx === 0) {
+          return "#ff0000";
+        }
         return "#0000ff";
       case EWowItemType.HERO:
+        if (idx === 0) {
+          return "#0000ff";
+        }
         return "#9900ff";
       case EWowItemType.MYTHIC:
+        if (idx === 0) {
+          return "#9900ff";
+        }
         return "black";
       default:
         if (itemLevelList && itemLevelList.length > 0) {
-          const itemLevel = itemLevelList.reduce((prev, curr) => {
+          const itemLevelInfo = itemLevelList.reduce((prev, curr) => {
             if ((item?.level ?? 0) >= curr.itemLevel) {
               return curr;
             } else {
               return prev;
             }
           }, itemLevelList[0]);
-          if (itemLevel.level <= 0) {
+          if (itemLevelInfo.level <= 0) {
             return "#00ff00";
-          } else if (itemLevel.level <= 4) {
+          } else if (itemLevelInfo.level <= 4) {
             return "#ff9900";
-          } else if (itemLevel.level <= 8) {
+          } else if (itemLevelInfo.level <= 8) {
             return "#ff0000";
-          } else if (itemLevel.level <= 12) {
+          } else if (itemLevelInfo.level <= 12) {
             return "#0000ff";
-          } else if (itemLevel.level <= 16) {
+          } else if (itemLevelInfo.level <= 16) {
             return "#9900ff";
           } else {
             return "black";
